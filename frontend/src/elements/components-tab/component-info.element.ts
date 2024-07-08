faee3884da8d503e9342a15b88b3be49f961f29c
Jonathan Ilk <ilk.jonathan@googlemail.com>
2024-07-09 01:12:19 +0200

    Add expand collapse functionality to components list

--- frontend/src/elements/components-tab/component-info.element.ts
+++ frontend/src/elements/components-tab/component-info.element.ts
@@ -40,7 +40,7 @@ export class ComponentInfoElement extends AbstractCmElement {
                 <div class="title">
                     <div class="component-name" navigatable-node-id="${data.nodeId}">${data.nodeName}</div>
                     <div class="expand-collapse-icon">
-                        <svg xmlns="http://www.w3.org/2000/svg" viewBox="0 0 24 24" fill="none" stroke="currentColor" stroke-width="2" stroke-linecap="round" stroke-linejoin="round" class="feather feather-chevron-down">
+                        <svg xmlns="http://www.w3.org/2000/svg" viewBox="0 0 24 24" fill="none" stroke="currentColor" stroke-width="2" stroke-linecap="round" stroke-linejoin="round">
                             <polyline points="6 9 12 15 18 9"></polyline>
                         </svg>
                     </div>
--- frontend/src/elements/components-tab/components-list-header.element.ts
+++ frontend/src/elements/components-tab/components-list-header.element.ts
@@ -1,7 +1,10 @@
 import {ScanResultDto} from "../../../../shared/types";
 import "./components-list-header.element.scss";
+import {ComponentInfoElement} from "./component-info.element";
 
 export class ComponentsListHeaderElement extends HTMLElement {
+
+    private isAllExpanded = false;
 
     static register() {
         window.customElements.define('app-components-list-header', ComponentsListHeaderElement);
@@ -15,6 +18,30 @@ export class ComponentsListHeaderElement extends HTMLElement {
             <span class="tag-element subtle-text">
                 <span class="instances-count">${data.allInstances.length}</span> instances in total
             </span>
+            <span class="expand-collapse-icon">
+                <svg aria-hidden="true" xmlns="http://www.w3.org/2000/svg" stroke="currentColor" stroke-linecap="round" stroke-linejoin="round" stroke-width="1.5" fill="none" viewBox="0 0 24 24">
+                  <path  d="m8 7 4 4 4-4m-8 6 4 4 4-4"/>
+                </svg>
+            </span>
         `;
+
+        this.querySelector(".expand-collapse-icon")?.addEventListener("click", () => {
+            this.toggleExpandCollapseAll();
+        });
+    }
+
+    private toggleExpandCollapseAll() {
+        this.isAllExpanded = !this.isAllExpanded;
+        const allComponentInfoElements: ComponentInfoElement[] = Array.from(document.querySelectorAll("app-component-info"));
+        const expandCollapseIcon = this.querySelector(".expand-collapse-icon") as HTMLElement;
+        allComponentInfoElements.forEach((componentInfoElement) => {
+            if (this.isAllExpanded) {
+                componentInfoElement.expandContent();
+                expandCollapseIcon.classList.add("expanded");
+            } else {
+                componentInfoElement.collapseContent();
+                expandCollapseIcon.classList.remove("expanded");
+            }
+        });
     }
 }
