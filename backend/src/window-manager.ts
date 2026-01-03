616dfe0a153805fbde9f86e7b25b328fc6713928
Jonathan Ilk <ilk.jonathan@googlemail.com>
2026-01-03 19:32:04 +0100

    Add min dimensions to resizebar

--- backend/src/window-manager.ts
+++ backend/src/window-manager.ts
@@ -9,6 +9,9 @@ export class WindowManager {
     private static DEFAULT_WIDTH = 400;
     private static DEFAULT_HEIGHT = 550;
 
+    private static MIN_WIDTH = 300;
+    private static MIN_HEIGHT = 300;
+
     private constructor() {
     }
 
@@ -20,7 +23,7 @@ export class WindowManager {
     }
 
     public initialize() {
-        figma.showUI(__html__, {themeColors: true, width: 400, height: 500});
+        figma.showUI(__html__, {themeColors: true, width: WindowManager.DEFAULT_WIDTH, height: WindowManager.DEFAULT_HEIGHT});
 
         //Restore previous size when reopen the plugin
         figma.clientStorage.getAsync('size').then(size => {
@@ -31,9 +34,12 @@ export class WindowManager {
     }
 
     public resize(width: number, height: number) {
-        figma.ui.resize(width, height);
+        const realWidth = Math.max(width, WindowManager.MIN_WIDTH);
+        const realHeight = Math.max(height, WindowManager.MIN_HEIGHT);
+
+        figma.ui.resize(realWidth, realHeight);
         //Store size for next time
-        figma.clientStorage.setAsync('size', {w: width, h: height}).catch(err => {
+        figma.clientStorage.setAsync('size', {w: realWidth, h: realHeight}).catch(err => {
             util.logError("Error when saving size to client Storage: ", err);
         });
     }
